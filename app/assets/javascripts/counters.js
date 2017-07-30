beacbcd20d9b5ec2a290d6580e0ab7ba05d1407c
Chamindu Salman <chamindu07@gmail.com>
2017-07-30 21:19:48 +0100

    rotated table 270def, and tried function to addClass to next td:child, onclick x is incremented by one and class is added to nth-child(x) but doesn't work for some reason

--- app/assets/javascripts/counters.js
+++ app/assets/javascripts/counters.js
@@ -1,3 +1,16 @@
-$('td').click(function() {
-  $(this).addClass('color-p1');
+var x = 0;
+$( document ).ready(function() {
+
+  $('tr').click(function() {
+  x = x + 1;
+  if ($(this).find('td:first-child').hasClass('color-p1')){
+    $('td', this).find('td:nth-child(x)').addClass('color-p1');
+  } else {
+    $(this).find('td:nth-child(1)').addClass('color-p1');
+  }});
+
 });
+/*
+if ($(this).hasClass('color-p1')){
+  $('tr:first-child').removeClass('color-p1');}
+else {*/
